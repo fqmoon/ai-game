ab2d268dcb615b93ace35ff8a0afbb678e0a58f1
njw <njw6622@outlook.com>
2021-09-29 22:02:26 +0800

    init scene

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import * as BABYLON from "babylonjs"
 
-let root = document.getElementById("root") as HTMLCanvasElement
+
+let canvas = document.getElementById("root") as HTMLCanvasElement
 
 
-let engine = new BABYLON.Engine(root)
-let scene = new BABYLON.Scene(engine)
+let engine = new BABYLON.Engine(canvas)
+
+
+const scene = new BABYLON.Scene(engine);
+
+const camera = new BABYLON.ArcRotateCamera(
+    "camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0),
+    scene);
+camera.attachControl(canvas, true);
+
+const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+const box = BABYLON.MeshBuilder.CreateBox("box", {});
+
+engine.runRenderLoop(function () {
+    if (scene && scene.activeCamera) {
+        scene.render();
+    }
+});
+
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
