ab6f99d1855002890b287111082f3de1170eb18e
njw <njw6622@outlook.com>
2021-09-29 21:49:07 +0800

    environment complete

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as BABYLON from "babylonjs"
+
+let root = document.getElementById("root") as HTMLCanvasElement
+
+
+let engine = new BABYLON.Engine(root)
+let scene = new BABYLON.Scene(engine)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "index.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: "source-map"
+};
