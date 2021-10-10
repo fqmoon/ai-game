f3763829b6e028a9c70a6244ec036cf5f83ad842
njw <njw6622@outlook.com>
2021-10-10 10:11:32 +0800

    重构拖拽系统（未完成）

--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,50 @@
 import * as BABYLON from "babylonjs";
 import {createSceneObjs} from "./sceneObjs";
+import {createDragController, DragEventData} from "./drag";
 
 export function createGame() {
     let canvas = document.getElementById("root") as HTMLCanvasElement
     let engine = new BABYLON.Engine(canvas)
-
-    let scene = new BABYLON.Scene(engine)
-    let camera = createCamera(scene, canvas)
-
-    let sceneObjs = createSceneObjs({scene,})
-
     // Resize
     window.addEventListener("resize", function () {
         engine.resize();
     });
+
+    let scene = new BABYLON.Scene(engine)
+    let camera = createCamera(scene, canvas)
+    let sceneObjs = createSceneObjs({scene})
+
+    let dragController = createDragController({scene, camera, canvas})
+    for (const human of sceneObjs.humans.values()) {
+        dragController.toDrags.add(human.mesh)
+    }
+    let dragInfos = new Map<BABYLON.AbstractMesh, {
+        originPos: BABYLON.Vector3,
+    }>()
+    dragController.onDragStartObservable.add(({draggingObj, pointerInfo}) => {
+        if (draggingObj) {
+            dragInfos.set(draggingObj, {originPos: draggingObj.position.clone()})
+        }
+    })
+    dragController.onDragEndObservable.add(({draggingObj, pointerInfo}) => {
+        if (!draggingObj)
+            return
+        let info = dragInfos.get(draggingObj)
+        if (!info)
+            return
+        draggingObj.position = info.originPos
+        dragInfos.clear()
+    })
+    dragController.onDragMoveObservable.add(({draggingObj, pointerInfo}) => {
+        if (!draggingObj)
+            return
+
+        let pos = getGroundPosition(scene, sceneObjs.ground)
+        if (pos) {
+            draggingObj.position = pos
+            draggingObj.position.y += 3
+        }
+    })
 
     return {
         engine,
@@ -40,3 +71,11 @@ function createCamera(scene: BABYLON.Scene, canvas: HTMLElement) {
 }
 
 export type Game = ReturnType<typeof createGame>
+
+// 获取当前鼠标所在的地形位置
+function getGroundPosition(scene: BABYLON.Scene, ground: BABYLON.AbstractMesh) {
+    let res = scene.pick(scene.pointerX, scene.pointerY, mesh => mesh === ground)
+    if (res && res.hit) {
+        return res.pickedPoint
+    }
+}
