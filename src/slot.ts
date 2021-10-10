12490aae2b1112eeb06f7cd1723249fb13b631df
njw <njw6622@outlook.com>
2021-10-10 15:49:16 +0800

    封装slot

---
+++ src/slot.ts
@@ -0,0 +1,122 @@
+import * as BABYLON from "babylonjs";
+
+export type SlotPos = [number, number]
+export type SlotSize = [number, number]
+
+/**
+ * 空间管理
+ * 只负责一个分布区域，即修道士放置区域算一个，野人的放置区域也算一个
+ */
+export function createSlotManager({leftDownPosition, rightUpPosition, slotSize}: {
+    leftDownPosition: BABYLON.Vector2,
+    rightUpPosition: BABYLON.Vector2,
+    // 槽的尺寸，以行列表示
+    slotSize: SlotSize,
+}) {
+    // 创建空间矩阵，位图表示法
+    function createSpaceMatrix() {
+        let spaceMat = new Array(slotSize[0])
+        // 行优先
+        for (let row = 0; row < slotSize[0]; row++) {
+            spaceMat[row] = new Array(slotSize[1]).fill(false)
+        }
+        return spaceMat
+    }
+
+    // 空间矩阵，位图表示法
+    let spaceMat = createSpaceMatrix()
+
+    // 已经占用的位置数量
+    let occupy = 0
+    let capacity = slotSize[0] * slotSize[1]
+
+    function calculatePosition(slotPos: [number, number]) {
+        function interpolate(min: number, max: number, count: number, index: number) {
+            return (max - min) / count * index + min
+        }
+
+        let x = interpolate(leftDownPosition.x, rightUpPosition.x, slotSize[0], slotPos[0])
+        let y = interpolate(leftDownPosition.y, rightUpPosition.y, slotSize[1], slotPos[1])
+        return [x, y]
+    }
+
+    function isEmpty() {
+        return occupy === 0
+    }
+
+    function isFull() {
+        if (occupy > capacity)
+            throw Error("容量已溢出")
+        else return occupy === capacity;
+    }
+
+    // 按条件搜索
+    function find(predication: { (space: boolean): boolean }): SlotPos | false {
+        for (let row = 0; row < slotSize[0]; row++) {
+            for (let col = 0; col < slotSize[1]; col++) {
+                if (predication(spaceMat[row][col]))
+                    return [row, col]
+            }
+        }
+
+        return false
+    }
+
+    function findOccupySlot(): SlotPos | false {
+        if (isEmpty())
+            return false
+
+        return find(space => space)
+    }
+
+    function findEmptySlot(): SlotPos | false {
+        if (isFull())
+            return false
+
+        return find(space => !space)
+    }
+
+    function put() {
+        let spacePos = findEmptySlot()
+        if (spacePos) {
+            let [row, col] = spacePos
+            spaceMat[row][col] = true
+            occupy += 1
+            return calculatePosition(spacePos)
+        }
+        return false
+    }
+
+    function pop() {
+        let spacePos = findOccupySlot()
+        if (spacePos) {
+            let [row, col] = spacePos
+            spaceMat[row][col] = false
+            occupy -= 1
+            return calculatePosition(spacePos)
+        }
+        return false
+    }
+
+    function arrange() {
+        let toOccupy = 0;
+        for (let row = 0; row < slotSize[0]; row++) {
+            for (let col = 0; col < slotSize[1]; col++) {
+                if (toOccupy < occupy) {
+                    spaceMat[row][col] = true
+                    ++toOccupy
+                } else {
+                    spaceMat[row][col] = false
+                }
+            }
+        }
+    }
+
+    return {
+        put,
+        pop,
+        arrange,
+    }
+}
+
+export type SlotManager = ReturnType<typeof createSlotManager>
