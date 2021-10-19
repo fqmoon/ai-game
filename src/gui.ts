24dbdcd788ce8e6336ffbeb9baadcda5cdb29f0e
njw <njw6622@outlook.com>
2021-10-19 23:25:34 +0800

    编写部分UI

--- src/gui.ts
+++ src/gui.ts
@@ -44,12 +44,52 @@ function createBoatLeaveButton({gameStatus, gameEvents, boat, humans}: {
     return button
 }
 
+function createGameOver({gameStatus, gameEvents, boat, humans}: {
+    gameEvents: GameEvents, gameStatus: GameStatus, boat: Region, humans: Iterable<Human>,
+}) {
+    // 这个div是全屏的阻止用户点击相关事件
+    let div = $.parseHTML(`<div 
+            style="position: absolute;width: 100%; height:100%;"
+        >
+            <div class="background-ui">
+                <div class="button-container">
+                    <button id="restart">重来</button>
+                </div>
+            </div>
+        </div>`)[0] as HTMLDivElement
+    return div
+}
+
+function injectCss(){
+    $('head').append(`<style type="text/css">
+        div{
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            position: absolute;
+        }
+        .background-ui{
+            margin: 15%;
+        }
+        #restart{
+            left: 1em;
+            bottom: 1em;
+            position: absolute;
+        }
+    </style>`)
+}
+
 export function createGUI({gameStatus, gameEvents, boat, humans}: {
     gameEvents: GameEvents, gameStatus: GameStatus, boat: Region, humans: Iterable<Human>,
 }) {
+    injectCss()
+
     let guiDiv = document.getElementById("gui") as HTMLDivElement
     guiDiv.style.backgroundColor = "transparent"
     guiDiv.append(createBoatLeaveButton({gameStatus, gameEvents, boat, humans}))
+    guiDiv.append(createGameOver({gameStatus, gameEvents, boat, humans}))
+
     return {
         rootDiv: guiDiv,
     }
