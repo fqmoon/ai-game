1cc7dad786a1dddb7f2f93dd595de0819f0a365b
njw <njw6622@outlook.com>
2021-10-24 23:59:42 +0800

    默认取消相机控制

--- src/camera.ts
+++ src/camera.ts
@@ -5,17 +5,22 @@ export function createCamera({scene, canvas, game}: {
     scene: BABYLON.Scene, canvas: HTMLElement, game: Game,
 }) {
     const camera = new BABYLON.ArcRotateCamera(
-        "camera", -Math.PI / 2, Math.PI / 3.5, 20, new BABYLON.Vector3(0, 0, 0),
+        "camera", -Math.PI * 0.5, Math.PI / 3.5, 20, new BABYLON.Vector3(0, 0, -3),
         scene);
-    camera.attachControl(canvas, true);
 
-    // 在拖拽时禁止相机旋转
-    game.humanDrag.onAfterDraggingStatusChangeObservable.add(status => {
-        if (status === 'draggingStart')
-            camera.detachControl()
-        else if (status === 'draggingEnd')
-            camera.attachControl()
-    })
+    // 仅在调试场景时附加相机控制
+    // @ts-ignore
+    if (window.debugScene) {
+        camera.attachControl(canvas, true);
+
+        // 在拖拽时禁止相机旋转
+        game.humanDrag.onAfterDraggingStatusChangeObservable.add(status => {
+            if (status === 'draggingStart')
+                camera.detachControl()
+            else if (status === 'draggingEnd')
+                camera.attachControl()
+        })
+    }
 
     return camera
 }
