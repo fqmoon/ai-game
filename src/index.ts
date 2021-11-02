b578645d3a1e59d9af75334714ae3e50201ab68e
njw <njw6622@outlook.com>
2021-11-02 11:09:45 +0800

    将CDN资源本地化

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import * as BABYLON from 'babylonjs'
+
+BABYLON.DracoCompression.Configuration = {
+    decoder: {
+        wasmUrl: "draco_wasm_wrapper_gltf.js",
+        wasmBinaryUrl: "draco_decoder_gltf.wasm",
+        fallbackUrl: "draco_decoder_gltf.js",
+    }
+};
+
 import {createGame} from "./game";
 
 async function main() {
